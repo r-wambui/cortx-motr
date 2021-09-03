94b31052cad66c0def336cba31b65e696bfec878
Nikita Danilov <nikita.danilov@seagate.com>
2021-09-03 13:00:01 +0300

    node_bulk_worker(): try timedwait.

--- net/test/node_bulk.c
+++ net/test/node_bulk.c
@@ -1287,7 +1287,9 @@ static void node_bulk_worker(struct node_bulk_ctx *ctx)
 		m0_net_test_nh_sd_copy_locked(&ctx->nbc_nh);
 		/* wait for STOP command or buffer event */
 		if (!ctx->nbc_callback_executed)
-			m0_chan_wait(&ctx->nbc_stop_clink);
+			m0_chan_timedwait(&ctx->nbc_stop_clink,
+					  m0_time_now() +
+					  M0_MKTIME(0, 100 * M0_TIME_ONE_MSEC));
 		if (running && node_bulk_is_stopping(ctx)) {
 			/* dequeue all queued network buffers */
 			node_bulk_buf_dequeue(ctx);
